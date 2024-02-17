b9218a9cf1f1e7668ba9582169bdbf48998a3e61
erfan <erfan01724@gmail.com>
2024-02-17 20:43:56 +0330

    creating first features of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:note/screen/HomeScreen.dart';
 
-import 'package:hive_flutter/adapters.dart';
-import 'package:note/Car.dart';
-import 'package:note/Student.dart';
-
-void main() async {
-  await Hive.initFlutter();
-  await Hive.openBox('names');
-  Hive.registerAdapter(CarAdapter());
-  await Hive.openBox<Car>('carBox');
-
-  Hive.registerAdapter(StudentAdapter());
-  await Hive.openBox<Student>('StudentBox');
-  runApp(Application());
+void main() {
+  runApp(
+    Application(),
+  );
 }
 
 class Application extends StatefulWidget {
@@ -23,85 +15,11 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
-  var textEditingController = TextEditingController();
-  var intputedText = '';
-  var box = Hive.box<Car>('carBox');
-  var studentBox = Hive.box<Student>('StudentBox');
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: textEditingController,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 2, color: Colors.red),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 2, color: Colors.red),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  studentBox.delete(1);
-                });
-              },
-              child: Text(
-                'delete',
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                studentBox.put(
-                  1,
-                  Student('ErfAn', 'Man', '195cm'),
-                );
-
-                studentBox.put(
-                  2,
-                  Student('Fatemeh', 'Woman', '177cm'),
-                );
-
-                setState(() {});
-              },
-              child: Text(
-                'Click for printing',
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (studentBox.get(1) == null) {
-                  return;
-                }
-
-                print(studentBox.get(1)!.name);
-                print(studentBox.get(1)!.gender);
-                print(studentBox.get(1)!.height);
-                print('');
-                print(studentBox.get(2)!.name);
-                print(studentBox.get(2)!.gender);
-                print(studentBox.get(2)!.height);
-              },
-              child: Text('read'),
-            ),
-          ]),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screen/HomeScreen.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 201, 201, 201),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                    ),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 100,
+                            child: Stack(
+                              alignment: Alignment.topLeft,
+                              children: [
+                                Container(
+                                  width: double.infinity,
+                                  color: Colors.white,
+                                ),
+                                Positioned(
+                                  left: 52,
+                                  top: 20,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'First',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              Color.fromARGB(255, 20, 225, 181),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      SizedBox(
+                                        width: 160,
+                                        child: Text(
+                                          'Secondaerfmaerfmeropmerpom,vaeropergoeprkgoop,wmvwm',
+                                          maxLines: 2,
+                                          style: TextStyle(fontSize: 13),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 10,
+                                  child: Transform.scale(
+                                    scale: 1.4,
+                                    child: Checkbox(
+                                      activeColor:
+                                          Color.fromARGB(255, 20, 225, 181),
+                                      value: true,
+                                      onChanged: (value) {},
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 30,
+                                  width: 80,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color.fromARGB(255, 20, 225, 181),
+                                  ),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '10:30',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(left: 6),
+                                          width: 20,
+                                          height: 20,
+                                          child: Image(
+                                            image: AssetImage(
+                                                'images/icon_time.png'),
+                                          ),
+                                        ),
+                                      ]),
+                                ),
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Container(
+                                  height: 30,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color.fromARGB(255, 210, 225, 222),
+                                  ),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Edit',
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 20, 225, 181),
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(left: 6),
+                                          width: 20,
+                                          height: 20,
+                                          child: Image(
+                                            image: AssetImage(
+                                                'images/icon_edit.png'),
+                                          ),
+                                        ),
+                                      ]),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  flex: 7,
+                ),
+                Expanded(
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/banking.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  flex: 4,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
