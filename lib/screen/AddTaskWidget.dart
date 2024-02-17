9c59af2e69690046ba21c61326a86db79b37423a
erfan <erfan01724@gmail.com>
2024-02-17 21:47:38 +0330

    creating FAB icon using floatingActionButton

---
+++ lib/screen/AddTaskWidget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddTaskWidget extends StatefulWidget {
+  const AddTaskWidget({super.key});
+
+  @override
+  State<AddTaskWidget> createState() => _AddTaskWidgetState();
+}
+
+class _AddTaskWidgetState extends State<AddTaskWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('add task'),
+      ),
+    );
+  }
+}
